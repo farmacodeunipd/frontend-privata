c9b481ea0314b1abdba913ac333e0f12e9d748c4
mattiapandolfo <102430265+mattiapandolfo@users.noreply.github.com>
2023-12-06 16:45:36 +0100

    style

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -19,23 +19,23 @@ function Filtro() {
                     <div>
                         <label
                             for="cars"
-                            className="text-black dark:text-white"
+                            className="text-black dark:text-white pr-1"
                         >
                             Choose a car:
                         </label>
-                        <select name="cars" id="cars">
+                        <select name="cars" id="cars" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                             <option value="0" selected disabled>
                                 Selezionare...
                             </option>
                             {cars.map((cars) => (
-                                <option value={cars.valore}>{cars.nome}</option>
+                                <option value={cars.valore} className="text-gray-700 hover:bg-gray-100 hover:text-gray-900">{cars.nome}</option>
                             ))}
                         </select>
                     </div>
                     <div>
                         <label
                             for="dogs"
-                            className="text-black dark:text-white"
+                            className="text-black dark:text-white pr-1"
                         >
                             Choose a car:
                         </label>
@@ -48,20 +48,20 @@ function Filtro() {
                         </datalist>
                     </div>
                     <div>
-                        <label for="top" className="text-black dark:text-white">
+                        <label for="top" className="text-black dark:text-white pr-1">
                             Choose a top:
                         </label>
-                        <select name="top" id="cars">
+                        <select name="top" id="cars" className="mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <option value="0" selected disabled>
                                 Selezionare...
                             </option>
                             {tops.map((tops) => (
-                                <option value={tops.valore}>{tops.nome}</option>
+                                <option value={tops.valore} className="text-gray-700 hover:bg-gray-100 hover:text-gray-900">{tops.nome}</option>
                             ))}
                         </select>
                     </div>
                     <div>
-                        <button type="button">Ricerca</button>
+                        <button type="button" className="bg-green-500 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">Ricerca</button>
                     </div>
                 </form>
             </div>
