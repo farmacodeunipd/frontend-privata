7ce4ca5a899ccc169fb96b422ea2a6067ea257ba
Ciro9320 <ciropassata93@gmail.com>
2023-12-13 21:59:38 +0100

    completamento api py

--- src/app.py
+++ src/app.py
@@ -1,31 +1,28 @@
 from flask import Flask, request, jsonify
-#from algoritmo import Algoritmo
-from algoritmo import A
 from ai_classes import Model, FileInfo
 
 app = Flask(__name__)
 
-a = A(model = Model(file_info=FileInfo(model_file='trained_model.pkl', file_path="/Users/alessandro_passarella/Desktop/progettoSWE/frontend-privata/src/output_file.csv", column_1='cod_cli', column_2='cod_art', column_3='qta_ordinata')))
-a.trainModel()
+model = Model(file_info=FileInfo(model_file='trained_model.pkl', file_path="output_file.csv", column_1='cod_cli', column_2='cod_art', column_3='qta_ordinata'))
+model.train_model()
 
 # Endpoint per la comunicazione con l'algoritmo
-@app.route('/esegui_algoritmo/<n>')
-def esegui_algoritmo_endpoint(n):
+@app.route('/esegui_algoritmo/<object>/<id>/<n>')
+def esegui_algoritmo_endpoint(object, id, n):
     try:
-        
-        risultato = a.top_5_utente(n)
-        
-        risultato = dict(risultato)
-
-        mio_dizionario = {}
-        for k,v in risultato.items() : 
-            mio_dizionario[k] = v
-
-        return jsonify(mio_dizionario)
+        if object == "user": 
+            dictionary = model.topN_1UserNItem(int(id), int(n))
+            dictionary = {str(uid): float(est) for uid, est in dictionary}
+        elif object == "item":
+            dictionary = model.topN_1ItemNUser(int(id), int(n))
+            dictionary = {str(uid): float(est) for uid, est in dictionary}
+        else:
+            return jsonify({'errore': "Wrong object. Select user or item"}), 501
+        return jsonify(dictionary)  
 
     except Exception as e:
         # Gestire eventuali errori
-        return jsonify({'errore': str(e)}), 501
+        return jsonify({'errore': str(e)}), 500
 
 # Avviare il server Flask
 if __name__ == '__main__':
